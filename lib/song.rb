f312f0b98e72718d52c78e0512bac8379bf666a2
Mike <mikewrubel51@gmail.com>
2021-11-03 13:59:12 -0400

    commit tree 926fbce76a7057377b2440a0ce79d60cb9db5709

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist 
+        @genre = genre
+        @@genres << genre
+        @@artists << artist
+        @@count += 1
+    end
+
+    def self.count
+       @@count
+    end 
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artist_count
+    end
+
+    def self.genre_count
+    end
+
+end
